3d1e49b934d6c8e1237f83cb0e534c4c3efee9f6
moscovig <moscovig@gmail.com>
2016-06-04 20:18:08 +1000

    wq

--- zehava/src/facedata.js
+++ zehava/src/facedata.js
@@ -2,8 +2,36 @@ import React, { PropTypes } from 'react';
 import styles from '../styles/facebook.scss';
 
 class FaceData extends React.Component { 
+
+  state = {details:''
+    
+  };
+
+  constructor(props) {
+    super(props);
+  
+    const {status,user_id,token} = this.props.conn;
+    this.user_id = user_id
+    this.token = token
+
+	this.getFBdata()
+  }
+	
+  	getFBdata(){
+		FB.api('/'+this.user_id,  {fields: ['last_name','first_name']},response => {
+			 
+			 	this.setState({fname:response.first_name,lname:response.last_name})
+			 				});
+
+	
+  	};
+
+
 	render(){
-				return(<h1>The App is ready</h1>);
+				return(<div >
+					<h1>Welcome {this.state.fname} {this.state.lname} </h1>
+				 <div ><img src={'http://graph.facebook.com/'+this.user_id+'/picture?type=large'} /></div>
+				 </div>);
 		};
 }
 
